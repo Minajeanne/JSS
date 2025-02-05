f3d20f76eaaea163d5d5213364301e1973f7de8a
Minajeanne <albrown07@mac.com>
2025-02-05 18:05:47 -0500

    add local env

--- acupuncture-booking/src/app/pages/index.js
+++ acupuncture-booking/src/app/pages/index.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { loadStripe } from '@stripe/stripe-js';
 import moment from 'moment';
-import styles from '../styles/Home.module.css'; // Create this!
+import styles from '../styles/Home.module.css'; // Create this! 
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);  //Secure Key
 
